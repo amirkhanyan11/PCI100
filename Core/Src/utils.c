63cb5b498fc7d5ae3a12cbf1f5b5970d5e9bf548
amirkhanyan11 <artyom.amirkhanyan11@gmail.com>
2025-03-17 12:21:44 +0400

    +dac

--- Core/Src/utils.c
+++ Core/Src/utils.c
@@ -18,7 +18,7 @@ static uint32_t start = 0;
 
 void uart_echo(void) {
     uint8_t rxbuf = 0;
-    if (HAL_OK == HAL_UART_Receive(&huart1, &rxbuf, 1, 10)){
+    if (HAL_OK == HAL_UART_Receive(&huart1, &rxbuf, 1, 10)) {
         HAL_UART_Transmit(&huart1, &rxbuf, 1, 10);
     }
 }
