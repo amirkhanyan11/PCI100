fb1ca4dcf0774a04b69ce6cec94e90e90c83b049
amirkhanyan11 <artyom.amirkhanyan11@gmail.com>
2025-04-09 17:48:22 +0400

    minor bug fix

--- Core/Src/app/app_config.c
+++ Core/Src/app/app_config.c
@@ -9,15 +9,15 @@
 #include "led.h"
 #include "pex.h"
 #include "adc.h"
-#include "eeprom.h"
 #include "cli_pex.h"
+#include "cli_eeprom.h"
 
 __attribute__((weak)) void app_config(app_t * const app)
 {
-	app_cmd_add(app, "led", 	&exec_led);
 	app_cmd_add(app, "dac",  	&cli_dac);
-	app_cmd_add(app, "help",	&exec_help);
 	app_cmd_add(app, "pex",  	&cli_pex);
-	app_cmd_add(app, "adc",  	&exec_adc);
 	app_cmd_add(app, "eeprom",  &cli_eeprom);
+	app_cmd_add(app, "help",	&exec_help);
+	app_cmd_add(app, "adc",  	&exec_adc);
+	app_cmd_add(app, "led", 	&exec_led);
 }
